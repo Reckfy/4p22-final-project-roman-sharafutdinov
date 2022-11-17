45cb79efafcd4d49e9b787fde942becc51437460
Roman <113034914+Reckfy@users.noreply.github.com>
2022-11-17 19:00:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
             <div className='content'>
                 {cartOpen ? <OverlayCart arrayCard={cartItems} onCloseCart={() => setCartOpen(false)} onRemove={onRemoveItem} /> : null}
                 
-                <HashRouter basename="/4p22-final-project-roman-sharafutdinov/tree/main">
+                <HashRouter basename="/4p22-final-project-roman-sharafutdinov">
                     <Routes>
                         <Route path="/" element={
                             <div>
